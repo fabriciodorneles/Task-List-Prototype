c794aedd0967a4192677a4efe2f12e06058213f8
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-06-13 20:33:05 -0300

    - First Commit

---
+++ tasklist.js
@@ -0,0 +1,14 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var tasks = [
+    'Fazer Café',
+    'Estudar Javascript',
+    'Atualizar Linkedin',
+    'Yoga e Ducha',
+];
+
+function renderTasks() {
+    
+}

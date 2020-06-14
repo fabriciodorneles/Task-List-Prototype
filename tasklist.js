090ad3ddad4bce1100e5a5ee0d307c1f18c69899
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-06-13 21:21:39 -0300

    - Insert and Exclude Items - Save in Local Storage

--- tasklist.js
+++ tasklist.js
@@ -2,13 +2,51 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var tasks = [
-    'Fazer Café',
-    'Estudar Javascript',
-    'Atualizar Linkedin',
-    'Yoga e Ducha',
-];
+
+
+var tasks = JSON.parse(localStorage.getItem('list_tasks')) || [];
+
 
 function renderTasks() {
-    
+    listElement.innerHTML = ''; //apaga tudo que estiver na list element
+    for(task of tasks){
+        var taskElement = document.createElement('li');
+        var taskText = document.createTextNode(task);
+        
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href','#');
+        var pos = tasks.indexOf(task); //retorna o indice do elemento que tem o mesmo parametro passado
+        linkElement.setAttribute('onclick', 'deleteTask(' + pos + ')');//seta a chamada de função com o index de cada elemento
+        var linkText = document.createTextNode('Excluir');
+        linkElement.appendChild(linkText);
+        
+        taskElement.appendChild(taskText);
+        taskElement.appendChild(linkElement);
+        
+        listElement.appendChild(taskElement);
+    }    
+}
+
+renderTasks();
+
+function addTask() {
+    var taskText = inputElement.value;
+
+    tasks.push(taskText);
+    inputElement.value = ''; //reset o valor do input
+    renderTasks();
+    saveToStorage();
+}
+
+buttonElement.onclick = addTask;
+
+function deleteTask(pos) {
+    tasks.splice(pos, 1); //remove uma quantidade de items do array baseado na posicao passada
+    renderTasks();
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('list_tasks', JSON.stringify(tasks));
+
 }
